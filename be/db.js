1a1c28ab45179fc307940ec8ae0aa5f88cd648c4
Ayush995712 <ayush181217@gmail.com>
2025-11-02 12:08:24 +0530

    Added timestamps in db.js and modified get method for users

--- be/db.js
+++ be/db.js
@@ -8,10 +8,9 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const user = new Schema({
-    user_id: ObjectId,
-    username: {type: String, default: "Jon Doe",required: true},
+    username: {type: String,required: true},
     email: {type: String,required: true, unique: true},
     password: {type: String, required: true}
-})
+}, { timestamps: true })
 
 export const userModel = mongoose.model('Users', user);
--- be/index.js
+++ be/index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
-import { userModel } from "./db";
+import { userModel } from "./db.js";
 
 dotenv.config();
 const admin_username = process.env.ADMIN_USERNAME;
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/", async (req, res) => {
+app.post("/api/users", async (req, res) => {
     const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
@@ -29,11 +29,27 @@ app.post("/", async (req, res) => {
     }
 })
 
-app.post("/admin", (req, res) => {
+app.post("/api/admin/login", async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     if(username == admin_username && password == admin_password) {
+        return res.status(200).json({
+            message: "Admin Signed in successfully"
+        })
+    }
+    return res.status(401).json({
+        message: "Invalid Credentials"
+    })
+})
 
+app.get("/api/admin/users", async (req, res) => {
+    try {
+        const users = await userModel.find();
+        res.json(users);
+    } catch(err) {
+        res.status(500).json({
+            message: err.message
+        })
     }
 })
 
